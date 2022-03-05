33a9b4e5684736480c78bcc16d8013ab801087d0
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-03-05 15:36:44 -0600

    commit tree a3b60ed6d17f5d205c5943093a2860390ed23311

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+        "name": name,
+        "email": email,
+        })
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(body => addContact(body.id))
+        .catch(function (error) {
+            let message = "Unauthorized Access";
+            alert("Unauthorized Access");
+            console.log(error.message);
+            document.body.append(message);
+        })
+}
+function addContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+submitData("name", "name@name.com");
